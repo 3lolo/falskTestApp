96ea8fe9380a821085b2a581c325e08800eeea04
Anatolii Pozniak <anatolip@pointer.com>
2020-11-06 15:51:35 +0200

    udpate

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from config import Config
+
 # from app.autoria.api.config import Config as AutoriaConfig
 
 app = Flask(__name__)
+
 # app.config.from_object(Config)
 # app.config.from_object(AutoriaConfig)
+from app import routes
 
 from app.autoria.api import auto_bp as auto_ria_api
 from app.autoria.api.errors import api_error_bp as auto_ria_api_error
